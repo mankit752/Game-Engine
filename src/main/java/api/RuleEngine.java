16b3eb518c1e7a3c11e23dcdbaf6489a854f209b
Ankit Maheshwari <ankitmaheshwari@Ankits-MacBook-Pro.local>
2024-07-08 17:44:47 +0530

    Removing repetation in Rule engine

--- src/main/java/api/RuleEngine.java
+++ src/main/java/api/RuleEngine.java
@@ -2,10 +2,8 @@ package api;
 
 import boards.TicTacToeBoard;
 import game.Board;
-import game.Cell;
 import game.GameState;
 
-import java.util.Iterator;
 import java.util.function.BiFunction;
 import java.util.function.Function;
 
@@ -14,19 +12,18 @@ public class RuleEngine {
     public GameState getState(Board board) {
 
         if (board instanceof TicTacToeBoard board1) {
-            String firstCharacter;
 
-            GameState rowWin = findStreak((i, j) -> board1.getSymbol(i, j));
-            if (rowWin != null) return rowWin;
+            GameState rowWin = outerTraversal((i, j) -> board1.getSymbol(i, j));
+            if (rowWin.isOver()) return rowWin;
 
-            GameState colWin = findStreak((i, j) -> board1.getSymbol(j, i));
-            if (colWin != null) return colWin;
+            GameState colWin = outerTraversal((i, j) -> board1.getSymbol(j, i));
+            if (colWin.isOver()) return colWin;
 
-            GameState diagWin = findDiagStreak(i -> board1.getSymbol(i, i));
-            if (diagWin != null) return diagWin;
+            GameState diagWin = traverse(i2 -> board1.getSymbol(i2, i2));
+            if (diagWin.isOver()) return diagWin;
 
-            GameState revDiagWin = findDiagStreak(i -> board1.getSymbol(i, 2 - i));
-            if (revDiagWin != null) return revDiagWin;
+            GameState revDiagWin = traverse(i1 -> board1.getSymbol(i1, 2 - i1));
+            if (revDiagWin.isOver()) return revDiagWin;
 
             int countOfFillerCells = 0;
             for (int i = 0; i < 3; i++) {
@@ -46,36 +43,32 @@ public class RuleEngine {
         }
     }
 
-    private GameState findDiagStreak(Function<Integer, String> diag) {
-        String firstCharacter = diag.apply(0);
-        boolean possibleStreak = firstCharacter != null;
+    private GameState outerTraversal(BiFunction<Integer, Integer, String> next) {
+        GameState result = new GameState(false, "-");
         for (int i = 0; i < 3; i++) {
-            if (diag.apply(0) != null && !diag.apply(0).equals(diag.apply(i))) {
+            int ii = i;
+            GameState traversal = traverse(j -> next.apply(ii, j));
+            if (traversal.isOver()) {
+                result = traversal;
+                break;
+            }
+        }
+        return result;
+    }
+
+    private GameState traverse(Function<Integer, String> traversal) {
+        GameState result = new GameState(false, "-");
+        boolean possibleStreak = true;
+        for (int j = 0; j < 3; j++) {
+            if (traversal.apply(j) == null || !traversal.apply(0).equals(traversal.apply(j))) {
                 possibleStreak = false;
                 break;
             }
         }
         if (possibleStreak) {
-            return new GameState(true, diag.apply(0));
+            result = new GameState(true, traversal.apply(0));
         }
-        return null;
-    }
-
-    private static GameState findStreak(BiFunction<Integer, Integer, String> next) {
-
-        for (int i = 0; i < 3; i++) {
-            boolean possibleStreak = true;
-            for (int j = 0; j < 3; j++) {
-                if (next.apply(i, j) == null || !next.apply(i, 0).equals(next.apply(i, j))) {
-                    possibleStreak = false;
-                    break;
-                }
-            }
-            if (possibleStreak) {
-                return new GameState(true, next.apply(i, 0));
-            }
-        }
-        return null;
+        return result;
     }
 
     @Override
--- src/test/java/GamePlayTest.java
+++ src/test/java/GamePlayTest.java
@@ -1,94 +1,94 @@
-import api.AIEngine;
 import api.GameEngine;
 import api.RuleEngine;
 import game.Board;
 import game.Cell;
 import game.Move;
 import game.Player;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class GamePlayTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+
+public class GamePlayTest {
     GameEngine gameEngine;
     RuleEngine ruleEngine;
 
     @BeforeEach
     public void setup() {
-        this.gameEngine = new GameEngine();
-        this.ruleEngine = new RuleEngine();
+        gameEngine = new GameEngine();
+        ruleEngine = new RuleEngine();
     }
 
     @Test
     public void checkForRowWin() {
         Board board = gameEngine.start("TicTacToe");
-        int[][] firstPlayerMoves = new int[][]{{1, 0}, {1, 1}, {1, 2}};
+        //make moves in a loop
+        int[][] moves = new int[][]{{1, 0}, {1, 1}, {1, 2}};
         int[][] secondPlayerMoves = new int[][]{{0, 0}, {0, 1}, {0, 2}};
-        playGame(board, firstPlayerMoves, secondPlayerMoves);
-        Assertions.assertTrue(ruleEngine.getState(board).isOver());
-        Assertions.assertEquals(ruleEngine.getState(board).getWinner(), "X");
+        playGame(board, moves, secondPlayerMoves);
+        assertTrue(ruleEngine.getState(board).isOver());
+        assertEquals(ruleEngine.getState(board).getWinner(), "X");
     }
 
     @Test
     public void checkForColWin() {
         Board board = gameEngine.start("TicTacToe");
-        int next = 0;
-        int[][] firstPlayerMoves = new int[][]{{0, 0}, {1, 0}, {2, 0}};
+        //make moves in a loop
+        int[][] moves = new int[][]{{0, 0}, {1, 0}, {2, 0}};
         int[][] secondPlayerMoves = new int[][]{{0, 1}, {0, 2}, {1, 1}};
-        playGame(board, firstPlayerMoves, secondPlayerMoves);
-        Assertions.assertTrue(ruleEngine.getState(board).isOver());
-        Assertions.assertEquals(ruleEngine.getState(board).getWinner(), "X");
+        playGame(board, moves, secondPlayerMoves);
+        assertTrue(ruleEngine.getState(board).isOver());
+        assertEquals(ruleEngine.getState(board).getWinner(), "X");
     }
 
     @Test
     public void checkForDiagWin() {
         Board board = gameEngine.start("TicTacToe");
-        int[][] firstPlayerMoves = new int[][]{{0, 0}, {1, 1}, {2, 2}};
-        int[][] secondPlayerMoves = new int[][]{{0, 1}, {0, 2}, {1, 2}};
-        playGame(board, firstPlayerMoves, secondPlayerMoves);
-        Assertions.assertTrue(ruleEngine.getState(board).isOver());
-        Assertions.assertEquals(ruleEngine.getState(board).getWinner(), "X");
+        //make moves in a loop
+        int[][] moves = new int[][]{{0, 0}, {1, 1}, {2, 2}};
+        int[][] secondPlayerMoves = new int[][]{{0, 1}, {0, 2}, {1, 0}};
+        playGame(board, moves, secondPlayerMoves);
+        assertTrue(ruleEngine.getState(board).isOver());
+        assertEquals(ruleEngine.getState(board).getWinner(), "X");
     }
 
     @Test
     public void checkForRevDiagWin() {
         Board board = gameEngine.start("TicTacToe");
-        int[][] firstPlayerMoves = new int[][]{{0, 2}, {1, 1}, {2, 0}};
+        //make moves in a loop
+        int[][] moves = new int[][]{{0, 2}, {1, 1}, {2, 0}};
         int[][] secondPlayerMoves = new int[][]{{0, 0}, {0, 1}, {1, 0}};
-        playGame(board, firstPlayerMoves, secondPlayerMoves);
-        Assertions.assertTrue(ruleEngine.getState(board).isOver());
-        Assertions.assertEquals(ruleEngine.getState(board).getWinner(), "X");
+        playGame(board, moves, secondPlayerMoves);
+        assertTrue(ruleEngine.getState(board).isOver());
+        assertEquals(ruleEngine.getState(board).getWinner(), "X");
     }
 
     @Test
     public void checkForSecondPlayerWin() {
         Board board = gameEngine.start("TicTacToe");
-        int[][] firstPlayerMoves = new int[][]{{1, 0}, {1, 1}, {2, 0}};
+        //make moves in a loop
+        int[][] moves = new int[][]{{1, 0}, {1, 1}, {2, 0}};
         int[][] secondPlayerMoves = new int[][]{{0, 0}, {0, 1}, {0, 2}};
-        playGame(board, firstPlayerMoves, secondPlayerMoves);
-        Assertions.assertTrue(ruleEngine.getState(board).isOver());
-        Assertions.assertEquals(ruleEngine.getState(board).getWinner(), "O");
+        playGame(board, moves, secondPlayerMoves);
+        assertTrue(ruleEngine.getState(board).isOver());
+       assertEquals(ruleEngine.getState(board).getWinner(), "O");
     }
 
     private void playGame(Board board, int[][] firstPlayerMoves, int[][] secondPlayerMoves) {
-
         int next = 0;
         while (!ruleEngine.getState(board).isOver()) {
-            Player second = new Player("O");
-            Player first = new Player("X");
-            System.out.println("Make your move!!");
-            System.out.println("Board = " + board);
+            Player first = new Player("X"), second = new Player("O");
             int row = firstPlayerMoves[next][0];
             int col = firstPlayerMoves[next][1];
             Move firstPlayerMove = new Move(new Cell(row, col), first);
             gameEngine.move(board, firstPlayerMove);
-
             if (!ruleEngine.getState(board).isOver()) {
                 int sRow = secondPlayerMoves[next][0];
                 int sCol = secondPlayerMoves[next][1];
-                Move secondPlayerMove = new Move(new Cell(sRow, sCol), second);
-                gameEngine.move(board, secondPlayerMove);
+                Move secondplayerMove = new Move(new Cell(sRow, sCol), second);
+                gameEngine.move(board, secondplayerMove);
             }
             next++;
         }
