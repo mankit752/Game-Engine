5af43b89a35b0f28d7bc79cd8ce5ebfe5a43e39e
Ankit Maheshwari <ankitmaheshwari@Ankits-MacBook-Pro.local>
2024-07-08 14:25:54 +0530

    Removing repetation in Rule engine

--- src/main/java/api/RuleEngine.java
+++ src/main/java/api/RuleEngine.java
@@ -2,77 +2,32 @@ package api;
 
 import boards.TicTacToeBoard;
 import game.Board;
+import game.Cell;
 import game.GameState;
+
+import java.util.Iterator;
+import java.util.function.BiFunction;
+import java.util.function.Function;
 
 public class RuleEngine {
 
     public GameState getState(Board board) {
 
         if (board instanceof TicTacToeBoard board1) {
+            String firstCharacter;
 
-            String firstCharacter = "-";
-            boolean rowComplete = true;
-            for (int i = 0; i < 3; i++) {
-                firstCharacter = board1.getSymbol(i, 0);
-                rowComplete = firstCharacter != null;
+            GameState rowWin = findStreak((i, j) -> board1.getSymbol(i, j));
+            if (rowWin != null) return rowWin;
 
-                if (firstCharacter != null) {
-                    for (int j = 1; j < 3; j++) {
-                        if (!firstCharacter.equals(board1.getSymbol(i, j))) {
-                            rowComplete = false;
-                            break;
-                        }
-                    }
-                }
-                if (rowComplete) break;
-            }
-            if (rowComplete) {
-                return new GameState(true, firstCharacter);
-            }
+            GameState colWin = findStreak((i, j) -> board1.getSymbol(j, i));
+            if (colWin != null) return colWin;
 
-            boolean colComplete = true;
-            for (int i = 0; i < 3; i++) {
-                firstCharacter = board1.getSymbol(0, i);
-                colComplete = firstCharacter != null;
+            GameState diagWin = findDiagStreak(i -> board1.getSymbol(i, i));
+            if (diagWin != null) return diagWin;
 
-                if (firstCharacter != null) {
-                    for (int j = 1; j < 3; j++) {
-                        if (!firstCharacter.equals(board1.getSymbol(j, i))) {
-                            colComplete = false;
-                            break;
-                        }
-                    }
-                }
-                if (colComplete) break;
-            }
-            if (colComplete) {
-                return new GameState(true, firstCharacter);
-            }
+            GameState revDiagWin = findDiagStreak(i -> board1.getSymbol(i, 2 - i));
+            if (revDiagWin != null) return revDiagWin;
 
-
-            firstCharacter = board1.getSymbol(0, 0);
-            boolean diagComplete = firstCharacter != null;
-            for (int i = 0; i < 3; i++) {
-                if (firstCharacter != null && !firstCharacter.equals(board1.getSymbol(i, i))) {
-                    diagComplete = false;
-                    break;
-                }
-            }
-            if (diagComplete) {
-                return new GameState(true, firstCharacter);
-            }
-
-            firstCharacter = board1.getSymbol(0, 2);
-            boolean revDiagComplete = firstCharacter != null;
-            for (int i = 0; i < 3; i++) {
-                if (firstCharacter != null && !firstCharacter.equals(board1.getSymbol(i, 2 - i))) {
-                    revDiagComplete = false;
-                    break;
-                }
-            }
-            if (revDiagComplete) {
-                return new GameState(true, firstCharacter);
-            }
             int countOfFillerCells = 0;
             for (int i = 0; i < 3; i++) {
                 for (int j = 0; j < 3; j++) {
@@ -89,6 +44,38 @@ public class RuleEngine {
         } else {
             return new GameState(false, "-");
         }
+    }
+
+    private GameState findDiagStreak(Function<Integer, String> diag) {
+        String firstCharacter = diag.apply(0);
+        boolean possibleStreak = firstCharacter != null;
+        for (int i = 0; i < 3; i++) {
+            if (diag.apply(0) != null && !diag.apply(0).equals(diag.apply(i))) {
+                possibleStreak = false;
+                break;
+            }
+        }
+        if (possibleStreak) {
+            return new GameState(true, diag.apply(0));
+        }
+        return null;
+    }
+
+    private static GameState findStreak(BiFunction<Integer, Integer, String> next) {
+
+        for (int i = 0; i < 3; i++) {
+            boolean possibleStreak = true;
+            for (int j = 0; j < 3; j++) {
+                if (next.apply(i, j) == null || !next.apply(i, 0).equals(next.apply(i, j))) {
+                    possibleStreak = false;
+                    break;
+                }
+            }
+            if (possibleStreak) {
+                return new GameState(true, next.apply(i, 0));
+            }
+        }
+        return null;
     }
 
     @Override
